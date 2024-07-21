dcbeffa7620c06990c9a665a6128c1e68efda04f
Teja Nayak <prasadnaik1092@gmail.com>
2024-07-21 23:22:45 +0530

    Login-Register API Developing  Completed

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import { connectDB } from './config/db.js';
 import foodRouter from './routes/FoodRoute.js';
-
+import userRouter from './routes/UserRoute.js';
+import 'dotenv/config'
 
 
 // app config
@@ -19,6 +20,8 @@ connectDB();
 // Api End Points
 app.use('/api/food', foodRouter)
 app.use('/images', express.static('uploads'))
+app.use('/api/user', userRouter);
+
 
 app.get("/", (req, res) => {
     res.send("API Working")
