50ccb27e78c9f742dabe2316dcd3d6069e3bffbc
Teja Nayak <prasadnaik1092@gmail.com>
2024-07-28 17:53:39 +0530

    api orders list for admin panel completed

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -83,4 +83,18 @@ const userOrders = async (req, res) => {
     }
 }
 
-export { placeOrder, verifyOrder, userOrders };
+// listing Orders For Admin Panel
+
+const listOrders = async (req, res) => {
+    try {
+        const orders = await orderModel.find({});
+        res.json({ success: true, data: orders })
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Error" })
+    }
+}
+
+
+
+export { placeOrder, verifyOrder, userOrders, listOrders };
